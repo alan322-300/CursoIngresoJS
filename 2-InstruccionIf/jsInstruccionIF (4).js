e5cdcfcca9d6876f63d42cfdfd5fb76ae7f0c01f
alan322-300 <52939179+alan322-300@users.noreply.github.com>
2019-07-15 22:26:59 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,13 @@
-function mostrar()
-{
+function mostrar(){
 //tomo la edad  
 
+var edad;
+edad= parseInt (document.getElementById("edad").value);
 
+if (edad >= 13 && edad <=17) {
+
+    alert("Es un adolescente");
+
+}
 
 }//FIN DE LA FUNCIÓN
